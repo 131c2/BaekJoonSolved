9274701b5287424eb17cc4f1d41b74089c0ee350
131c2 <bg3189@naver.com>
2025-04-10 22:16:55 +0900

    [Silver II] Title: 약수의 합 2, Time: 180 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
@@ -0,0 +1,34 @@
+/*
+    x >= 모든 자연수 y의 약수의 합  => g(x)
+    g(N) => N보다 작은 자연수들의 약수의 합?
+    N  g(N)
+    1   1
+    2   4?
+    10  87
+    2 1
+    2 1 1
+    10 5      2 1        18
+    9       3   1        13
+    8     4   2 1        15
+    7           1        8
+    6       3 2 1        12
+    5           1        6
+    4         2 1        7
+    3           1        4
+    2           1        3
+    1                    1
+*/
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n, i;
+        long sum = 0;
+        n = sc.nextInt();
+        
+        for(i = 1 ; i <= n ; i ++){
+            sum += (long) (i * (n / i));
+        }
+        System.out.println(sum);
+    }
+}
